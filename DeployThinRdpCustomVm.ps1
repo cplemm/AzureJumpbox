c0dfceea53df6d5fcab3e89831bc56553c6ffb70
Carsten Lemm <clemm@microsoft.com>
2015-11-20 16:11:29 +0100

    Initial version

---
+++ DeployThinRdpCustomVm.ps1
@@ -0,0 +1,8 @@
+﻿# using Azure PowerShell 1.0
+Login-AzureRmAccount
+
+$group = "thinrdpcustom"
+New-AzureRmResourceGroup -Name $group -Location "West Europe"
+New-AzureRmResourceGroupDeployment -ResourceGroupName $group -TemplateFile ".\ThinRdpVmCustomImage.json" -TemplateParameterFile ".\ThinRdpVmCustomImage.param.json" 
+
+
